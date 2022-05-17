603683c6739509779d72fd22005bf8d81e641646
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-05-17 11:27:45 -0300

    games.js

--- Games.js
+++ Games.js
@@ -1,10 +1,6 @@
 const Sequelize = require("sequelize")
 const connection = require("./database")
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3b76275eb9b0f4a4c58106dfb7881d2f023b88e8
 const games = connection.define('games', {
     
   id: { type: Sequelize.INTEGER, allowNull: false, primaryKey: true },
